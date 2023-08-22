6b0b0d458b6a129216811a74f8004f9410d5a3aa
Eugene Lian <eugene.lian.2022@smu.edu.sg>
2023-08-22 11:24:33 +0800

    Starting Maven Project

---
+++ backend/src/test/java/com/cs203/g1t4/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs203.g1t4.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
