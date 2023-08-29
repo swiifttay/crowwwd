53b334b18ed873a68d6e0254e6442f02cecd98b1
jtrabino <jtrabino.2022@scis.smu.edu.sg>
2023-08-29 14:11:03 +0800

    refactor: Update User model

--- backend/src/main/java/com/cs203/g1t4/backend/models/User.java
+++ backend/src/main/java/com/cs203/g1t4/backend/models/User.java
@@ -1,9 +1,6 @@
 package com.cs203.g1t4.backend.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -13,10 +10,12 @@ import jakarta.validation.constraints.Size;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+@Getter
 @Document("user")
 @NoArgsConstructor
 @AllArgsConstructor
@@ -26,13 +25,59 @@ public class User implements UserDetails {
     @Id
     private String id;
 
+    @Getter
+    @NotBlank
+    private String firstName;
+
+    @Getter
+    @NotBlank
+    private String lastName;
+
+    @NotBlank
+    private String username;
+
+    @Getter
     @NotBlank
     @Email
     private String email;
 
+    @Getter
     @NotBlank
     @Size(max = 120)
     private String password;
+
+    @Getter
+    @NotBlank
+    private String phoneNo;
+
+    @Getter
+    private LocalDateTime userCreationDate;
+
+    @Getter
+    private String nationality;
+
+    @Getter
+    private String countryOfResidence;
+
+    @Getter
+    private String countryCode;
+
+    @Getter
+    private String gender;
+
+    @Getter
+    private String dateOfBirth;
+
+    @Getter
+    private String address;
+
+    @Getter
+    private String postalCode;
+
+    private boolean isPreferredMarketing;
+
+    @Getter
+    private String spotifyAccount;
 
     public User(String email, String password) {
         this.email = email;
@@ -48,7 +93,75 @@ public class User implements UserDetails {
 
     @Override
     public String getUsername() {
-        return email;
+        return username;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public void setUserCreationDate(LocalDateTime userCreationDate) {
+        this.userCreationDate = userCreationDate;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public void setCountryOfResidence(String countryOfResidence) {
+        this.countryOfResidence = countryOfResidence;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public boolean isPreferredMarketing() {
+        return isPreferredMarketing;
+    }
+
+    public void setPreferredMarketing(boolean preferredMarketing) {
+        isPreferredMarketing = preferredMarketing;
+    }
+
+    public void setSpotifyAccount(String spotifyAccount) {
+        this.spotifyAccount = spotifyAccount;
     }
 
     @Override
