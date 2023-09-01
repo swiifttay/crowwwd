8a575fda02a337b69d2ee4f7f28cbfa441230eee
yutinggh <huang.yu.tingting@gmail.com>
2023-09-01 15:53:22 +0800

    feat: signup page

--- frontend/src/pages/signup.tsx
+++ frontend/src/pages/signup.tsx
@@ -3,21 +3,40 @@ import RootLayout from "@/app/layout";
 import backgroundImg from "../../public/images/Singer.jpg";
 import Link from "next/link";
 
-function Login() {
+function Signup() {
   return (
     <RootLayout>
       <div
         className="flex flex-col items-center justify-center min-h-screen py-2"
         style={{
-          backgroundImage: `url(${backgroundImg})`, //idk how to make the background img show up...
+          backgroundImage: `url(${backgroundImg})`,
           backgroundSize: "cover",
           backgroundPosition: "center",
         }}
       >
         <main className="flex relative flex-col items-center justify-center w-full flex-1 px-20 text-center lg:left-1/4 sm:w-[95vw] xs:w-[90vw]">
-          <div className="text-4xl font-bold mb-4">Welcome to Crowd Sync</div>
-          <p className="text-lg">Please log in to access your account.</p>
+          <div className="text-4xl font-bold mb-4">Join Crowd Sync</div>
+          <p className="text-lg">Create your account to get started.</p>
+
           <form className="mt-8 w-full max-w-sm">
+            <div className="flex space-x-2">
+              <div className="flex-1 mb-4">
+                <input
+                  type="text"
+                  id="firstName"
+                  className="mt-1 px-3 py-2 w-full border border-zinc-500 rounded-lg text-white bg-theme-midnight"
+                  placeholder="First Name"
+                />
+              </div>
+              <div className="flex-1 mb-4">
+                <input
+                  type="text"
+                  id="lastName"
+                  className="mt-1 px-3 py-2 w-full border border-zinc-500 rounded-lg text-white bg-theme-midnight"
+                  placeholder="Last Name"
+                />
+              </div>
+            </div>
             <div className="mb-4">
               <input
                 type="text"
@@ -31,23 +50,23 @@ function Login() {
                 type="password"
                 id="password"
                 className="mt-1 px-3 py-2 w-full border border-zinc-500 rounded-lg text-white bg-theme-midnight"
-                placeholder="Enter your password"
+                placeholder="Create a password"
               />
             </div>
             <button
               type="submit"
               className="mt-6 w-full bg-theme-blue text-white py-2 rounded-lg hover:bg-theme-light-blue"
             >
-              Log In
+              Sign Up
             </button>
           </form>
           <div className="mt-8 text-md flex space-x-2">
-            <div className="text-theme-grey">New to CROWD SYNC?</div>
+            <div className="text-theme-grey">Already have an account?</div>
             <Link
-              href="/signup"
+              href="/login"
               className="text-theme-blue hover:text-theme-light-blue"
             >
-              Sign up.
+              Log in.
             </Link>
           </div>
         </main>
@@ -56,4 +75,4 @@ function Login() {
   );
 }
 
-export default Login;
+export default Signup;
