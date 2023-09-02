10b6fd00c9ebe65378be205f777551bc0fe821c2
owjoel <owjoel@gmail.com>
2023-09-02 10:53:24 +0800

    chore: removed unneeded dependencies

--- frontend/src/app/components/Navbar.tsx
+++ frontend/src/app/components/Navbar.tsx
@@ -2,54 +2,46 @@
 
 import Image from "next/image";
 import { MagnifyingGlassIcon } from "@heroicons/react/20/solid";
-import { useState } from "react";
 import Link from "next/link";
-// import RootLayout from "@/app/layout";
 import Logo from "/public/images/Logo.png";
 
 export default function Navbar() {
-  const [searchActivated, setSearchActivated] = useState(false);
-
   return (
-    // <RootLayout>
-      //I changed sticky to fixed here
-      <div className="w-full font-semibold fixed p-[20px] md:px-[128px] justify-between flex flex-grow text-xl items-center z-50">
-        <div className="hover:text-gray-300 cursor-pointer">Explore</div>
-        <div className="relative group">
-          <a href="" className=" hover:text-gray-300">
-            About Us
-          </a>
-          <div
-            className="hidden group-hover:block w-40 bg-opacity-70 my-2
+    //I changed sticky to fixed here
+    <div className="w-full font-semibold fixed p-[20px] md:px-[128px] justify-between flex flex-grow text-xl items-center z-50">
+      <div className="hover:text-gray-300 cursor-pointer">Explore</div>
+      <div className="relative group">
+        <a href="" className=" hover:text-gray-300">
+          About Us
+        </a>
+        <div
+          className="hidden group-hover:block w-40 bg-opacity-70 my-2
               transition-opacity duration-300 absolute z-10"
-          >
-            <ul>
-              <li>
-                <a href="" className="hover:text-gray-300">
-                  Our Verticals
-                </a>
-              </li>
-              <li>
-                <a href="" className="hover:text-gray-300">
-                  FAQs
-                </a>
-              </li>
-            </ul>
-          </div>
+        >
+          <ul>
+            <li>
+              <a href="" className="hover:text-gray-300">
+                Our Verticals
+              </a>
+            </li>
+            <li>
+              <a href="" className="hover:text-gray-300">
+                FAQs
+              </a>
+            </li>
+          </ul>
         </div>
-        <Link href="/">
-          <Image src={Logo} alt="Logo" className="w-60 h-20 object-cover" />
-        </Link>
-        <div className="flex">
-          <MagnifyingGlassIcon className="h-6 stroke-white hover:text-gray-300 cursor-pointer mr-2" />
-          <div className="hover:text-gray-300 cursor-pointer">Search</div>
-        </div>
-        <Link href="/login">
-          <div className="hover:text-gray-300 cursor-pointer">
-            Login/Register
-          </div>
-        </Link>
       </div>
-    // </RootLayout>
+      <Link href="/">
+        <Image src={Logo} alt="Logo" className="w-60 h-20 object-cover" />
+      </Link>
+      <div className="flex">
+        <MagnifyingGlassIcon className="h-6 stroke-white hover:text-gray-300 cursor-pointer mr-2" />
+        <div className="hover:text-gray-300 cursor-pointer">Search</div>
+      </div>
+      <Link href="/login">
+        <div className="hover:text-gray-300 cursor-pointer">Login</div>
+      </Link>
+    </div>
   );
 }
