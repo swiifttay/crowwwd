299dc9e58552c9fc6ad1be0424f008e14d3ea168
yutinggh <huang.yu.tingting@gmail.com>
2023-09-05 08:49:43 +0800

    feat: personal info page (first edit)

---
+++ frontend/src/app/info/page.tsx
@@ -0,0 +1,78 @@
+"use client";
+
+import RootLayout from "@/app/layout";
+import React, { useState } from "react";
+import { COUNTRIES } from "../components/CountryPicker/countries";
+import CountrySelector from "../components/CountryPicker/selector";
+
+function Info() {
+  const myRef = React.createRef<HTMLDivElement>();
+
+  const [isOpen, setIsOpen] = useState(false);
+  // Default this to a country's code to preselect it
+  const [country, setCountry] = useState("AF");
+
+  return (
+    <RootLayout>
+      <div className="flex flex-col items-center justify-center min-h-screen py-2">
+        <main className="flex relative flex-col justify-center w-full flex-1 px-20 text-center">
+          <form className="mt-8 w-full max-w-sm">
+            <div className="">
+              <input
+                type="text"
+                id="title"
+                className="mt-1 px-3 py-2 w-full border border-zinc-500 rounded-lg text-white bg-theme-midnight mb-4"
+                placeholder="Title"
+              />
+              <input
+                type="text"
+                id="firstName"
+                className="mt-1 px-3 py-2 w-full border border-zinc-500 rounded-lg text-white bg-theme-midnight mb-4"
+                placeholder="First Name"
+              />
+              <input
+                type="text"
+                id="lastName"
+                className="mt-1 px-3 py-2 w-full border border-zinc-500 rounded-lg text-white bg-theme-midnight mb-4"
+                placeholder="Last Name"
+              />
+              <input
+                type="text"
+                id="nationality"
+                className="mt-1 px-3 py-2 w-full border border-zinc-500 rounded-lg text-white bg-theme-midnight mb-4"
+                placeholder="Nationality"
+              />
+              <input
+                type="text"
+                id="Country"
+                className="mt-1 px-3 py-2 w-full border border-zinc-500 rounded-lg text-white bg-theme-midnight mb-4"
+                placeholder="Country of Residence"
+              />
+            </div>
+            <CountrySelector
+              id={"countries"}
+              open={isOpen}
+              onToggle={() => setIsOpen(!isOpen)}
+              onChange={(val) => setCountry(val)}
+              // We use this type assertion because we are always sure this find will return a value but need to let TS know since it could technically return null
+              selectedValue={
+                COUNTRIES.find(
+                  (option) => option.value === country
+                ) as SelectMenuOption
+              }
+            />
+
+            <button
+              type="submit"
+              className="mt-6 w-full bg-theme-blue text-white py-2 rounded-lg hover:bg-theme-light-blue"
+            >
+              Save
+            </button>
+          </form>
+        </main>
+      </div>
+    </RootLayout>
+  );
+}
+
+export default Info;
