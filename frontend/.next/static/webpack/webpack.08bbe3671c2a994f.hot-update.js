102f28c7afdcf2f62ec15e30ef2c037e0a74975c
dexterlim24 <dexterlim24@gmail.com>
2023-10-10 22:48:27 +0800

    fix: sidepanel transition

--- frontend/.next/static/webpack/webpack.08bbe3671c2a994f.hot-update.js
+++ frontend/.next/static/webpack/webpack.08bbe3671c2a994f.hot-update.js
@@ -96,7 +96,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "1728d92f1e81777b"; }
+/******/ 	__webpack_require__.h = function() { return "d62062270bd7ee53"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/global */
