221a140de416a8090bf41218f6b7faad4f7a4f6b
dexterlim24 <dexterlim24@gmail.com>
2023-10-16 21:55:43 +0800

    fix: added vertical margin

--- frontend/.next/static/webpack/webpack.08bbe3671c2a994f.hot-update.js
+++ frontend/.next/static/webpack/webpack.08bbe3671c2a994f.hot-update.js
@@ -96,7 +96,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "d62062270bd7ee53"; }
+/******/ 	__webpack_require__.h = function() { return "4b1a0bb9deda9348"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/global */
--- frontend/src/app/payment/page.tsx
+++ frontend/src/app/payment/page.tsx
@@ -34,7 +34,7 @@ export default function App() {
   };
 
   return (
-    <div className="w-full h-full p-5 flex space-x-10">
+    <div className="w-full p-5 flex space-x-10 my-20">
       <div className="w-1/2 mb-10 rounded-3xl bg-checkout bg-cover bg-center flex flex-col justify-center items-center shadow-lg shadow-slate-950">
         <h1 className="m-2 text-5xl font-semibold text-center">
           You're on your way!
