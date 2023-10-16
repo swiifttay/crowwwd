894fae3182f6a0c582cb2539f730a4363f82709d
Eugene Lian <111040612+eugenelian@users.noreply.github.com>
2023-10-16 13:09:23 +0800

    chore: Clean up unused imports

--- backend/src/main/java/com/cs203/g1t4/backend/repository/SeatingDetailsRepository.java
+++ backend/src/main/java/com/cs203/g1t4/backend/repository/SeatingDetailsRepository.java
@@ -1,7 +1,5 @@
 package com.cs203.g1t4.backend.repository;
 
-import com.cs203.g1t4.backend.models.FanRecord;
-import com.cs203.g1t4.backend.models.event.Event;
 import com.cs203.g1t4.backend.models.event.EventSeatingDetails;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
