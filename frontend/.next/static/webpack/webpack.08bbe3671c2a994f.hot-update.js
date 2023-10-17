5d658a8e1686ef62995a15be3ea23e3e8add786d
yutinggh <huang.yu.tingting@gmail.com>
2023-10-17 09:54:50 +0800

    feat: minor styling for waiting room

--- frontend/.next/static/webpack/webpack.08bbe3671c2a994f.hot-update.js
+++ frontend/.next/static/webpack/webpack.08bbe3671c2a994f.hot-update.js
@@ -96,7 +96,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "7e1f23b974467cf3"; }
+/******/ 	__webpack_require__.h = function() { return "c79103a66b3fdcac"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/global */
--- frontend/src/app/components/WaitingRoom/Stepper.tsx
+++ frontend/src/app/components/WaitingRoom/Stepper.tsx
@@ -12,75 +12,11 @@ export default function HorizontalLinearAlternativeLabelStepper() {
     <Box sx={{ width: "100%" }}>
       <Stepper activeStep={1} alternativeLabel>
         {steps.map((label) => (
-          <Step
-            key={label}
-            sx={{ "&.MuiStepLabel-labelContainercolor": { color: "white" } }}
-          >
-            <StepLabel sx={{ "&.MuiStepLabel-root": { color: "white" } }}>
-              {label}
-            </StepLabel>
+          <Step key={label}>
+            <StepLabel>{label}</StepLabel>
           </Step>
         ))}
       </Stepper>
     </Box>
   );
 }
-
-// "use client"
-
-// import Box from "@mui/material/Box";
-// import Stepper from "@mui/material/Stepper";
-// import Step from "@mui/material/Step";
-// import StepLabel from "@mui/material/StepLabel";
-// import { styled } from "@mui/system";
-
-// const steps = [
-//   "Select master blaster campaign settings",
-//   "Create an ad group",
-//   "Create an ad",
-// ];
-
-// const StyledStepLabel = styled(StepLabel)({
-//   "&.MuiStepLabel-root": {
-//     color: "white",
-//   },
-// });
-
-// export default function HorizontalLinearAlternativeLabelStepper() {
-//   return (
-//     <Box sx={{ width: "100%" }}>
-//       <Stepper activeStep={1} alternativeLabel>
-//         {steps.map((label) => (
-//           <Step key={label}>
-//             <StyledStepLabel>{label}</StyledStepLabel>
-//           </Step>
-//         ))}
-//       </Stepper>
-//     </Box>
-//   );
-// }
-
-// import Box from "@mui/material/Box";
-// import Stepper from "@mui/material/Stepper";
-// import Step from "@mui/material/Step";
-// import StepLabel from "@mui/material/StepLabel";
-
-// const steps = [
-//   "Select master blaster campaign settings",
-//   "Create an ad group",
-//   "Create an ad",
-// ];
-
-// export default function HorizontalLinearAlternativeLabelStepper() {
-//   return (
-//     <Box sx={{ width: "100%" }}>
-//       <Stepper activeStep={1} alternativeLabel>
-//         {steps.map((label) => (
-//           <Step key={label}>
-//             <StepLabel style={{ color: "white" }}>{label}</StepLabel>
-//           </Step>
-//         ))}
-//       </Stepper>
-//     </Box>
-//   );
-// }
