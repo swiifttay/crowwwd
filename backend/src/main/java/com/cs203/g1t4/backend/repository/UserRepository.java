bf497e9ca3c28e185cdd7d46dd478811dd4e721c
jtrabino <jtrabino.2022@scis.smu.edu.sg>
2023-10-18 10:17:37 +0800

    test: added tests for TicketService

--- backend/src/main/java/com/cs203/g1t4/backend/repository/UserRepository.java
+++ backend/src/main/java/com/cs203/g1t4/backend/repository/UserRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.Optional;
 
 public interface UserRepository extends MongoRepository<User, String> {
-    Optional<User> findByUsername(String email);
 
-
+    Optional<User> findByUsername(String username);
 }
