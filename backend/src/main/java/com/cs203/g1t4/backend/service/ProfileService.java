64fb68923f133675b9678672ae7cad0d9c45d969
swiifttay <126874041+swiifttay@users.noreply.github.com>
2023-10-19 17:54:28 +0800

    fix: user spotify account connection

--- backend/src/main/java/com/cs203/g1t4/backend/service/ProfileService.java
+++ backend/src/main/java/com/cs203/g1t4/backend/service/ProfileService.java
@@ -1,16 +1,20 @@
 package com.cs203.g1t4.backend.service;
 
+import java.time.LocalDateTime;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
 import com.cs203.g1t4.backend.data.request.user.UpdateProfileRequest;
 import com.cs203.g1t4.backend.data.response.Response;
 import com.cs203.g1t4.backend.data.response.common.SuccessResponse;
 import com.cs203.g1t4.backend.data.response.user.SingleUserResponse;
 import com.cs203.g1t4.backend.models.User;
-import com.cs203.g1t4.backend.models.exceptions.*;
+import com.cs203.g1t4.backend.models.exceptions.InvalidTokenException;
 import com.cs203.g1t4.backend.repository.UserRepository;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
@@ -67,5 +71,45 @@ public class ProfileService {
         return SingleUserResponse.builder()
                 .user(user)
                 .build();
+    }
+
+    /**
+     * Validates the spotify user that is connected with the user that is logged in 
+     * 
+     * @param spotifyUserId a String object containing the userId of the spotify user that logged in
+     * @param username a String object containing the logged in user's username
+     * @return boolean value true if this is the first time the loggedin user connects to spotify
+     *                                  or if the spotify account user matches the loggedin user 
+     *                          false if the spotify account user does not match the loggedin user
+     */
+    public boolean validate(String spotifyUserId, String username) {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new InvalidTokenException());
+
+        if (user.getSpotifyAccount() == null) {
+                User updatedUser = User.builder()
+                        .id(user.getId())
+                        .firstName(user.getFirstName())
+                        .lastName(user.getLastName())
+                        .username(username)
+                        .email(user.getEmail())
+                        .phoneNo(user.getPhoneNo())
+                        .userCreationDate(LocalDateTime.now())
+                        .countryOfResidence(user.getCountryOfResidence())
+        //                .dateOfBirth(userRequest.getDateOfBirth())
+                        .address(user.getAddress())
+                        .postalCode(user.getPostalCode())
+                        .city(user.getCity())
+                        .state(user.getState())
+                        .isPreferredMarketing(user.isPreferredMarketing())
+                        .spotifyAccount(spotifyUserId)
+                        .build();
+                
+                userRepository.save(updatedUser);
+
+                return true;
+        }
+
+        return user.getSpotifyAccount().equals(spotifyUserId);
     }
 }
