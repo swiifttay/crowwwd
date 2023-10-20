87a90a9df810a01f1628e7b7339b275ccf3a7ea2
swiifttay <126874041+swiifttay@users.noreply.github.com>
2023-10-20 15:37:44 +0800

    test: AuthenticationServiceImpl complete

--- backend/src/test/java/com/cs203/g1t4/backend/AuthenticationServiceTest.java
+++ backend/src/test/java/com/cs203/g1t4/backend/AuthenticationServiceTest.java
@@ -3,35 +3,33 @@ package com.cs203.g1t4.backend;
 
 import com.cs203.g1t4.backend.data.request.user.AuthenticationRequest;
 import com.cs203.g1t4.backend.data.request.user.RegisterRequest;
-import com.cs203.g1t4.backend.data.response.Response;
+import com.cs203.g1t4.backend.data.request.user.UserRequest;
 import com.cs203.g1t4.backend.data.response.common.SuccessResponse;
 import com.cs203.g1t4.backend.data.response.user.AuthenticationResponse;
 import com.cs203.g1t4.backend.models.User;
 import com.cs203.g1t4.backend.models.exceptions.DuplicatedUsernameException;
+import com.cs203.g1t4.backend.models.exceptions.InvalidCredentialsException;
 import com.cs203.g1t4.backend.repository.UserRepository;
 import com.cs203.g1t4.backend.service.AuthenticationServiceImpl;
 import com.cs203.g1t4.backend.service.CommonService;
 import com.cs203.g1t4.backend.service.JwtService;
-import io.jsonwebtoken.io.Decoders;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.test.util.ReflectionTestUtils;
-import java.security.Key;
-import io.jsonwebtoken.security.Keys;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -41,65 +39,24 @@ public class AuthenticationServiceTest {
     @Mock
     private UserRepository userRepository;
 
-//    @Value("${crowwwd.backend.app.jwtSecretKey}")
-//    private String JWT_SECRET_KEY;
-
+    @Mock
     private JwtService jwtService;
 
-//    private Key signInKey;
-//    private String jwtToken;
-
+    @Mock
     private CommonService commonService;
 
-    private AuthenticationServiceImpl authenticationServiceImpl;
-    @Mock
-    private Response defaultResponse;
     @Mock
     private PasswordEncoder passwordEncoder;
     @Mock
-    private  AuthenticationManager authenticationManager;
+    private AuthenticationManager authenticationManager;
+
+    private AuthenticationServiceImpl authenticationServiceImpl;
+    private User existingUser;
 
     @BeforeEach
-    void setUp(){
-        jwtService = new JwtService();
-//        signInKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode("secret-key"));
-//        jwtToken = "jwt-token";
-
-        commonService = new CommonService(
-                defaultResponse,
-                jwtService,
-                passwordEncoder,
-                userRepository,
-                authenticationManager
-        );
-        authenticationServiceImpl = new AuthenticationServiceImpl(
-                userRepository,
-                passwordEncoder,
-                jwtService,
-                authenticationManager,
-                commonService
-        );
-    }
-
-    @Test
-    void register_NewUser_ReturnSuccessResponse() {
-        // arrange
-        RegisterRequest registerRequest = RegisterRequest.builder()
-                .firstName("Alice")
-                .lastName("Tan")
-                .username("AliceTan")
-                .email("aliceTan@test.com")
-                .password("12345678")
-                .phoneNo("91234567")
-                .userCreationDate(LocalDateTime.now())
-                .countryOfResidence("Singapore")
-                .address("Sentosa Cove Avenue 1")
-                .postalCode("S123456")
-                .city("Singapore")
-                .state("Singapore")
-                .build();
-
-        User user = User.builder()
+    void setUp() {
+        // arrange for all tests
+        existingUser = User.builder()
                 .id("1234")
                 .firstName("Alice")
                 .lastName("Tan")
@@ -117,18 +74,66 @@ public class AuthenticationServiceTest {
                 .spotifyAccount(null)
                 .build();
 
-        // mock userRepository "findByUsername" operation
-        when(userRepository.findByUsername(any(String.class))).thenReturn(Optional.empty());
+        // mock AuthenticationServiceImpl
+        authenticationServiceImpl = new AuthenticationServiceImpl(
+                userRepository,
+                passwordEncoder,
+                jwtService,
+                authenticationManager,
+                commonService
+        );
+    }
+
+    @Test
+    void register_NewUser_ReturnSuccessResponse() {
+        // arrange
+        RegisterRequest registerRequest = RegisterRequest.builder()
+                .firstName("Bob")
+                .lastName("Tan")
+                .username("BobTan")
+                .email("bobTan@test.com")
+                .password("12345678")
+                .phoneNo("91234567")
+                .userCreationDate(LocalDateTime.now())
+                .countryOfResidence("Singapore")
+                .address("Sentosa Cove Avenue 1")
+                .postalCode("S123456")
+                .city("Singapore")
+                .state("Singapore")
+                .build();
+
+        User newUser = User.builder()
+                .id("1234")
+                .firstName("Bob")
+                .lastName("Tan")
+                .username("BobTan")
+                .email("bobTan@test.com")
+                .password("12345678")
+                .phoneNo("91234567")
+                .userCreationDate(LocalDateTime.now())
+                .countryOfResidence("Singapore")
+                .address("Sentosa Cove Avenue 1")
+                .postalCode("S123456")
+                .city("Singapore")
+                .state("Singapore")
+                .isPreferredMarketing(false)
+                .spotifyAccount(null)
+                .build();
+
+
+        // mock commonService "getUserFromClass" operation
+        when(commonService.getUserClassFromRequest(any(UserRequest.class), eq(null))).thenReturn(newUser);
 
         // mock "save" operation
-        when(userRepository.save(any(User.class))).thenReturn(user);
+        when(userRepository.save(any(User.class))).thenReturn(newUser);
 
         // act
         SuccessResponse response = authenticationServiceImpl.register(registerRequest);
 
         // assert
         assertEquals("User has been created successfully", response.getResponse());
-        verify(userRepository).findByUsername(registerRequest.getUsername());
+        verify(commonService).getUserClassFromRequest(registerRequest, null);
+        verify(userRepository).save(newUser);
     }
 
 
@@ -150,26 +155,10 @@ public class AuthenticationServiceTest {
                 .state("Singapore")
                 .build();
 
-        User existingUser = User.builder()
-                .id("1234")
-                .firstName("Alice")
-                .lastName("Tan")
-                .username("AliceTan")
-                .email("aliceTan@test.com")
-                .password("12345678")
-                .phoneNo("91234567")
-                .userCreationDate(LocalDateTime.now())
-                .countryOfResidence("Singapore")
-                .address("Sentosa Cove Avenue 1")
-                .postalCode("S123456")
-                .city("Singapore")
-                .state("Singapore")
-                .isPreferredMarketing(false)
-                .spotifyAccount(null)
-                .build();
+        DuplicatedUsernameException duplicatedUsernameException = new DuplicatedUsernameException("AliceTan");
 
-        // mock userRepository "findByUsername" operation
-        when(userRepository.findByUsername(any(String.class))).thenReturn(Optional.of(existingUser));
+        // mock commonService "getUserFromClass" operation
+        when(commonService.getUserClassFromRequest(any(UserRequest.class), eq(null))).thenThrow(duplicatedUsernameException);
 
         DuplicatedUsernameException e = assertThrows(DuplicatedUsernameException.class, () -> {
             authenticationServiceImpl.register(registerRequest);
@@ -177,68 +166,71 @@ public class AuthenticationServiceTest {
 
         // assert
         assertEquals("User AliceTan exists", e.getMessage());
-        verify(userRepository).findByUsername(registerRequest.getUsername());
+        verify(commonService).getUserClassFromRequest(registerRequest, null);
     }
 
-//    @Test
-//    void authenticate_CorrectCredentials_ReturnAuthenticationResponse() {
-//        // arrange
-//        User existingUser = User.builder()
-//                .id("1234")
-//                .firstName("Alice")
-//                .lastName("Tan")
-//                .username("AliceTan")
-//                .email("aliceTan@test.com")
-//                .password("12345678")
-//                .phoneNo("91234567")
-//                .userCreationDate(LocalDateTime.now())
-//                .countryOfResidence("Singapore")
-//                .address("Sentosa Cove Avenue 1")
-//                .postalCode("S123456")
-//                .city("Singapore")
-//                .state("Singapore")
-//                .isPreferredMarketing(false)
-//                .spotifyAccount(null)
-//                .build();
-//
-//        AuthenticationRequest authenticationRequest = AuthenticationRequest.builder()
-//                .username("AliceTan")
-//                .password("12345678")
-//                .build();
-//
-//        // Mock the authenticationManager to not throw BadCredentialsException
-//        when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class))).thenReturn(null);
-//
-//        // Mock the behavior of userRepository.findByUsername to return the user
-//        when(userRepository.findByUsername(authenticationRequest.getUsername())).thenReturn(Optional.of(existingUser));
-//
-//        // Mock the behavior of jwtService to return a token
-//        when(jwtService.generateToken(existingUser)).thenReturn("your-token");
-//
-//        // Act
-//        AuthenticationResponse response = authenticationServiceImpl.authenticate(authenticationRequest);
-//
-//        // Assert
-//        assertNotNull(response);
-//        assertNotNull(response.getToken());
-//        assertTrue(response.getToken().equals("your-token"));
-//
-////        // mock userRepository "findByUsername" operation
-////        when(userRepository.findByUsername(any(String.class))).thenReturn(Optional.of(existingUser));
-////
-////        // act
-////        AuthenticationResponse authenticationResponse = authenticationServiceImpl.authenticate(authenticationRequest);
-////
-////        // assert
-////        assertEquals("your-token", authenticationResponse.getToken());
-////        verify(userRepository).findByUsername(authenticationRequest.getUsername());
-//
-//    }
+    @Test
+    void authenticate_CorrectCredentials_ReturnAuthenticationResponse() {
+
+        // arrange
+        AuthenticationRequest authenticationRequest = AuthenticationRequest.builder()
+                .username("AliceTan")
+                .password("12345678")
+                .build();
+
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+                new UsernamePasswordAuthenticationToken(
+                        authenticationRequest.getUsername(),
+                        authenticationRequest.getPassword()
+                );
+
+        // Mock the authenticationManager to not throw BadCredentialsException
+        when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class))).thenReturn(null);
+
+        // Mock the behavior of userRepository.findByUsername to return the user
+        when(userRepository.findByUsername(authenticationRequest.getUsername())).thenReturn(Optional.of(existingUser));
+
+        // Mock the behaviour of generateToken in jwtService
+        when(jwtService.generateToken(any(User.class))).thenReturn("your-token");
+
+        // Act
+        AuthenticationResponse response = authenticationServiceImpl.authenticate(authenticationRequest);
+
+        // Assert
+        assertTrue(response.getToken().equals("your-token"));
+        verify(authenticationManager).authenticate(usernamePasswordAuthenticationToken);
+        verify(userRepository).findByUsername(existingUser.getUsername());
+        verify(jwtService).generateToken(existingUser);
+    }
+
+    @Test
+    void authenticate_IncorrectCredentials_ReturnAuthenticationResponse() {
+
+        // arrange
+        AuthenticationRequest authenticationRequest = AuthenticationRequest.builder()
+                .username("AliceTan")
+                .password("87654321") // wrong password
+                .build();
+
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+                new UsernamePasswordAuthenticationToken(
+                        authenticationRequest.getUsername(),
+                        authenticationRequest.getPassword()
+                );
+
+        when(authenticationManager.authenticate(any(UsernamePasswordAuthenticationToken.class))).thenThrow(BadCredentialsException.class);
+        // Act
+        InvalidCredentialsException e = assertThrows(InvalidCredentialsException.class, () -> {
+            authenticationServiceImpl.authenticate(authenticationRequest);
+        });
+
+        // Assert
+    }
 
     @Test
     void findUsername_NewUsername_ReturnSuccessResponse() {
         // arrange
-        String username = "AliceTan";
+        String username = "CatherineLim";
 
         // mock userRepository "findByUsername" operation
         when(userRepository.findByUsername(any(String.class))).thenReturn(Optional.empty());
@@ -251,37 +243,20 @@ public class AuthenticationServiceTest {
         verify(userRepository).findByUsername(username);
     }
 
-
     @Test
     void findUsername_DuplicateUsername_ThrowDuplicateUsernameException() {
         // arrange
         String username = "AliceTan";
 
-        User existingUser = User.builder()
-                .id("1234")
-                .firstName("Alice")
-                .lastName("Tan")
-                .username("AliceTan")
-                .email("aliceTan@test.com")
-                .password("12345678")
-                .phoneNo("91234567")
-                .userCreationDate(LocalDateTime.now())
-                .countryOfResidence("Singapore")
-                .address("Sentosa Cove Avenue 1")
-                .postalCode("S123456")
-                .city("Singapore")
-                .state("Singapore")
-                .isPreferredMarketing(false)
-                .spotifyAccount(null)
-                .build();
-
         // mock userRepository "findByUsername" operation
         when(userRepository.findByUsername(any(String.class))).thenReturn(Optional.of(existingUser));
 
-        // assert
+        // act
         DuplicatedUsernameException e = assertThrows(DuplicatedUsernameException.class, () -> {
             authenticationServiceImpl.findUsername(username);
         });
+
+        // assert
         verify(userRepository).findByUsername(username);
     }
 
