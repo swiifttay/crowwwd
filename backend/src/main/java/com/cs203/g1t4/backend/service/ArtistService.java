981a80a7681b48329cce8272c6798fdd87f17149
swiifttay <126874041+swiifttay@users.noreply.github.com>
2023-10-22 17:56:02 +0800

    test:spotify fanrecord updates

--- backend/src/main/java/com/cs203/g1t4/backend/service/ArtistService.java
+++ backend/src/main/java/com/cs203/g1t4/backend/service/ArtistService.java
@@ -136,7 +136,7 @@ public class ArtistService {
 
         // check if there is already the artist
         if (currentArtist.isPresent()) {
-            String currentArtistId = updateSpotifyArtistByName(artist);
+            String currentArtistId = updateSpotifyArtistByName(artist, currentArtist.get().getId());
             return currentArtistId;
         }
 
@@ -148,13 +148,10 @@ public class ArtistService {
     }
 
     // for the purpose of updating artist when there is the old one to be replaced by spotify's information
-    public String updateSpotifyArtistByName(Artist newArtist) {
-
-        Artist originalArtist = artistRepository.findByName(newArtist.getName())
-                .orElseThrow(() -> new InvalidArtistIdException(newArtist.getName()));
+    public String updateSpotifyArtistByName(Artist newArtist, String originalArtistId) {
 
         Artist updatedArtist = Artist.builder()
-                .id(originalArtist.getId())
+                .id(originalArtistId)
                 .name(newArtist.getName())
                 .description(newArtist.getDescription())
                 .website(newArtist.getWebsite())
@@ -163,7 +160,7 @@ public class ArtistService {
 
         artistRepository.save(updatedArtist);
 
-        return updatedArtist.getId();
+        return originalArtistId;
     }
 
     public void artistRequestChecker(ArtistRequest artistRequest, Artist oldArtist) {
