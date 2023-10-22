de1bf883aa4aa9f61089be597f5252e64a232ade
swiifttay <126874041+swiifttay@users.noreply.github.com>
2023-10-22 08:55:11 +0800

    fix: artist image update

--- backend/src/main/java/com/cs203/g1t4/backend/service/serviceImpl/ArtistServiceImpl.java
+++ backend/src/main/java/com/cs203/g1t4/backend/service/serviceImpl/ArtistServiceImpl.java
@@ -28,18 +28,28 @@ public class ArtistServiceImpl implements ArtistService {
     private String bucketName;
 
     // for manually adding a artist for event creation
-    public Response addArtist(ArtistRequest request) {
+    public Response addArtist(ArtistRequest request, MultipartFile image) {
+
+        String artistImageName = image.getOriginalFilename();
 
         // create artist object
         Artist artist = Artist.builder()
                 .name(request.getName())
                 .description(request.getDescription())
                 .website(request.getWebsite())
-                .artistImage(request.getArtistImage())
+                .artistImage(artistImageName)
                 .build();
 
         // save into repo
         artistRepository.save(artist);
+
+        // Put the image into the bucket
+        s3Service.putObject(
+                bucketName,
+                "artist-images/%s/%s".formatted(artist.getId(), artistImageName),
+                image
+        );
+
 
         // return
         return SuccessResponse.builder()
@@ -63,7 +73,10 @@ public class ArtistServiceImpl implements ArtistService {
     }
 
 
-    public Response updateArtistById(String artistId, ArtistRequest request) {
+    public Response updateArtistById(String artistId, ArtistRequest request, MultipartFile image) {
+
+        // find the old artist
+        Artist artist = artistRepository.findById(artistId).orElseThrow(() -> new InvalidArtistIdException(artistId));
 
         // update information about the artist
         Artist newArtist = Artist.builder()
@@ -71,7 +84,20 @@ public class ArtistServiceImpl implements ArtistService {
                 .name(request.getName())
                 .description(request.getDescription())
                 .website(request.getWebsite())
+                .artistImage(artist.getArtistImage())
+                .artistImageURL(artist.getArtistImageURL())
                 .build();
+
+        if (image != null && !image.isEmpty()) {
+            // Put the image into the bucket
+            s3Service.putObject(
+                    bucketName,
+                    "artist-images/%s/%s".formatted(artistId, artist.getArtistImage()),
+                    image
+            );
+            // reset the artist image url in the event the previous set one was from spotify
+            artist.setArtistImageURL("https://%s.s3.ap-southeast-1.amazonaws.com/artist-images/%s/%s".formatted(bucketName, artist.getId(), artist.getArtistImage()));
+        }
 
         // save the artist
         artistRepository.save(newArtist);
@@ -82,31 +108,14 @@ public class ArtistServiceImpl implements ArtistService {
                 .build();
     }
 
-    // for the purpose of updating artist when there is call to them again
-    public String updateArtistByName(Artist newArtist) {
-
-        Artist originalArtist = artistRepository.findByName(newArtist.getName())
-                .orElseThrow(() -> new InvalidArtistIdException(newArtist.getName()));
-
-        Artist updatedArtist = Artist.builder()
-                .id(originalArtist.getId())
-                .name(newArtist.getName())
-                .description(newArtist.getDescription())
-                .website(newArtist.getWebsite())
-                .artistImageURL(newArtist.getArtistImageURL())
-                .build();
-
-        artistRepository.save(updatedArtist);
-
-        return updatedArtist.getId();
-    }
-
-    public Response findArtistById(String artistId) {
+    public Response getArtistById(String artistId) {
 
         Artist artist = artistRepository.findById(artistId)
                 .orElseThrow(() -> new InvalidArtistIdException(artistId));
 
-        artist.setArtistImageURL(getArtistImage(artistId));
+        if (artist.getArtistImage() != null) {
+            artist.setArtistImage("https://%s.s3.ap-southeast-1.amazonaws.com/artist-images/%s/%s".formatted(bucketName, artist.getId(), artist.getArtistImage()));
+        }
 
         return SingleArtistResponse.builder()
                 .artist(artist)
@@ -118,78 +127,14 @@ public class ArtistServiceImpl implements ArtistService {
         List<Artist> artistList = artistRepository.findAll();
 
         for (Artist artist : artistList) {
-            artist.setArtistImageURL(getArtistImage(artist.getId()));
+            if (artist.getArtistImage() != null) {
+                artist.setArtistImageURL("https://%s.s3.ap-southeast-1.amazonaws.com/artist-images/%s/%s".formatted(bucketName, artist.getId(), artist.getArtistImage()));
+            }
         }
 
         return ArtistResponse.builder()
                 .artists(artistList)
                 .build();
-    }
-
-    // for manual changing of picture
-    public SuccessResponse uploadArtistImage(String artistId,
-                                            MultipartFile multipartFile) {
-        // Get information on which artist to edit from
-        Artist artist = artistRepository.findById(artistId).orElseThrow(() -> new InvalidArtistIdException(artistId));
-
-        // Get the artist image name
-        String artistImageName = artist.getArtistImage();
-
-        // Check if this is to update image of the artist or to input a new one
-        if (artistImageName == null) {
-            artistImageName = multipartFile.getOriginalFilename();
-        }
-
-        // Put the image into the bucket
-        s3Service.putObject(
-                bucketName,
-                "artist-images/%s/%s".formatted(artistId, artistImageName),
-                multipartFile
-        );
-
-        //Update information on the Artist Image Name if it changed previously
-        Artist newArtist = Artist.builder()
-                .id(artist.getId())
-                .name(artist.getName())
-                .artistImage(artistImageName)
-                .artistImageURL("artist-images/%s/%s".formatted(artistId, artistImageName))
-                .description(artist.getDescription())
-                .build();
-
-        // Save to repository
-        artistRepository.save(newArtist);
-
-        // Return the success response if the image was saved successfully
-        return SuccessResponse.builder()
-                .response("Artist image has been uploaded successfully")
-                .build();
-    }
-
-    // depending on whether you are finding the image by spotify or by s3
-    public SuccessResponse getArtistImageResponse(String artistId) {
-
-        String artistImageUrl = getArtistImage(artistId);
-
-        // Return
-        return SuccessResponse.builder()
-                .response("Artist Image URL: " + artistImageUrl)
-                .build();
-    }
-
-    public String getArtistImage(String artistId) {
-
-        // Get information on which artist to edit from
-        Artist artist = artistRepository.findById(artistId).orElseThrow(() -> new InvalidArtistIdException(artistId));
-
-        String artistImageUrl = artist.getArtistImageURL();
-
-        // check if you are getting via s3 or spotify link
-        if ( artistImageUrl == null || artistImageUrl.length() == 0) {
-            artistImageUrl = "https://%s.s3.ap-southeast-1.amazonaws.com/artist-images/%s/%s".formatted(bucketName, artistId, artist.getArtistImage());
-        }
-
-        // Return
-        return artistImageUrl;
     }
 
     // helper method
@@ -208,4 +153,24 @@ public class ArtistServiceImpl implements ArtistService {
         // Get information on which artist to edit from
         return newArtist.getId();
     }
+
+    // for the purpose of updating artist when there is the old one to be replaced by spotify's information
+    public String updateArtistByName(Artist newArtist) {
+
+        Artist originalArtist = artistRepository.findByName(newArtist.getName())
+                .orElseThrow(() -> new InvalidArtistIdException(newArtist.getName()));
+
+        Artist updatedArtist = Artist.builder()
+                .id(originalArtist.getId())
+                .name(newArtist.getName())
+                .description(newArtist.getDescription())
+                .website(newArtist.getWebsite())
+                .artistImageURL(newArtist.getArtistImageURL())
+                .build();
+
+        artistRepository.save(updatedArtist);
+
+        return updatedArtist.getId();
+    }
+
 }
