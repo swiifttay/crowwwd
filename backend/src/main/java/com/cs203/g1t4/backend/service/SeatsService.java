e2bca9247fd1c3743555a9f1fd4ea10d29ed5984
Eugene Lian <111040612+eugenelian@users.noreply.github.com>
2023-10-23 13:41:18 +0800

    refactor: Helper method, final, etc

--- backend/src/main/java/com/cs203/g1t4/backend/service/SeatsService.java
+++ backend/src/main/java/com/cs203/g1t4/backend/service/SeatsService.java
@@ -28,9 +28,9 @@ public class SeatsService {
     private static final int NUM_ROWS = 4;
 
     public Response findSeats(final String eventId, final String category, final String numSeats)
-            throws InvalidSeatingDetailsException {
+            throws InvalidCategoryException, InvalidSeatingDetailsException {
 
-        //Create Seats to store information
+        //Create Seats to store information to be returned to frontend
         Seats seats = Seats.builder()
                 .category(category)
                 .build();
@@ -40,31 +40,22 @@ public class SeatsService {
                 .orElseThrow(() -> new InvalidSeatingDetailsException(eventId));
 
         //Obtain pricing for each seat
-        List<Category> categoryList = eventSeatingDetails.getCategories();
-        Category c = null;
-        boolean found = false;
-        for (int i = 0 ; i < categoryList.size() && !found ; i++) {
-            c = categoryList.get(i);
-            if (c.getCategory().equals(category)) {
-                found = true;
-                seats.setPricePerSeat(c.getPrice());
-            }
-        }
+        Category cat = findCategory(eventSeatingDetails, category);
 
-        //If Category is not in the list, throws an InvalidCategoryException back
-        if (!found) { throw new InvalidCategoryException(category); }
+        //Obtain the pricePerSeat from cat
+        seats.setPricePerSeat(cat.getPrice());
 
         //Calculate total cost of seats and update in seats
         seats.setTotalCost(seats.getPricePerSeat() * Integer.parseInt(numSeats));
 
         //Find seats using Seats Allocation
-        List<String> seatAllocation = getSeats(4, c.getSeatsInformationString());
+        List<String> seatAllocation = getSeats(Integer.parseInt(numSeats), cat.getSeatsInformationString());
 
         //Throws seatAllocation == null
         if (seatAllocation == null) { throw new IllegalArgumentException("No Combination can be found"); }
 
         //Update Seats Allocation Pending status into the seatingDetails
-        String updatedSeatsString = returnUpdatedSeatsString(c.getSeatsInformationString(), seatAllocation, '2');
+        String updatedSeatsString = returnUpdatedSeatsString(cat.getSeatsInformationString(), seatAllocation, '2');
         seatingDetailsService.updateSeatingDetails(eventId, category, updatedSeatsString);
 
         //Update Seating allocation into seats
@@ -79,25 +70,14 @@ public class SeatsService {
                         .build();
     }
 
-    public Response confirmSeats(String eventId, String category, SeatRequest seatRequest) {
+    public Response confirmSeats(final String eventId, final String category, final SeatRequest seatRequest) {
 
         //Find EventId in EventSeatingDetails, else throws InvalidSeatingDetailsException(eventId)
         EventSeatingDetails eventSeatingDetails = seatingDetailsRepository.findEventSeatingDetailsByEventId(eventId)
                 .orElseThrow(() -> new InvalidSeatingDetailsException(eventId));
 
-        //Obtain pricing for each seat
-        List<Category> categoryList = eventSeatingDetails.getCategories();
-        Category c = null;
-        boolean found = false;
-        for (int i = 0 ; i < categoryList.size() && !found ; i++) {
-            c = categoryList.get(i);
-            if (c.getCategory().equals(category)) {
-                found = true;
-            }
-        }
-
-        //If Category is not in the list, throws an InvalidCategoryException back
-        if (!found) { throw new InvalidCategoryException(category); }
+        //Find Category
+        Category c = findCategory(eventSeatingDetails, category);
 
         //Update Seats Allocation Pending status into the seatingDetails
         String updatedSeatsString = returnUpdatedSeatsString(c.getSeatsInformationString(),
@@ -111,12 +91,45 @@ public class SeatsService {
                 .build();
     }
 
-    public static List<String> getSeats(int numTickets, String seatsInformationString) {
+    public Response cancelSeats(final String eventId, final String category, final SeatRequest seatRequest) {
+
+        //Find EventId in EventSeatingDetails, else throws InvalidSeatingDetailsException(eventId)
+        EventSeatingDetails eventSeatingDetails = seatingDetailsRepository.findEventSeatingDetailsByEventId(eventId)
+                .orElseThrow(() -> new InvalidSeatingDetailsException(eventId));
+
+        //Find Category
+        Category c = findCategory(eventSeatingDetails, category);
+
+        //Update Seats Allocation Pending status into the seatingDetails
+        String updatedSeatsString = returnUpdatedSeatsString(c.getSeatsInformationString(),
+                seatRequest.getAllocatedSeats(), '0');
+        seatingDetailsService.updateSeatingDetails(eventId, category, updatedSeatsString);
+
+        //Consider returning ticket here?
+
+        return SuccessResponse.builder()
+                .response("Seats cancelled.")
+                .build();
+    }
+
+    private Category findCategory(final EventSeatingDetails eventSeatingDetails, final String category)
+            throws InvalidCategoryException{
+        List<Category> categoryList = eventSeatingDetails.getCategories();
+        for (int i = 0 ; i < categoryList.size(); i++) {
+            Category cat = categoryList.get(i);
+            if (cat.getCategory().equals(category)) {
+                return cat;
+            }
+        }
+        throw new InvalidCategoryException(category);
+    }
+
+    private List<String> getSeats(int numTickets, String seatsInformationString) {
         List<Integer> combination = getCombi(numTickets);
         return getSeats(numTickets, seatsInformationString, combination);
     }
 
-    public static List<String> getSeats(int numTickets, String seatsInformationString, List<Integer> combination) {
+    private List<String> getSeats(int numTickets, String seatsInformationString, List<Integer> combination) {
         if (combination == null) {
             return null;
         }
@@ -151,7 +164,7 @@ public class SeatsService {
 
     // positive result: represents the starting num in the seatsInformationString
     // negative result: no suitable seat found
-    public static int find(int numTickets, String seatsInformationString) {
+    private int find(int numTickets, String seatsInformationString) {
         // find the first occurrence of the empty seats
         int firstEmpty = seatsInformationString.indexOf('0');
 
@@ -185,13 +198,13 @@ public class SeatsService {
     }
 
     // initial getCombi
-    public static List<Integer> getCombi(int numConsecutiveTickets) {
+    private List<Integer> getCombi(int numConsecutiveTickets) {
 
         return Arrays.asList(numConsecutiveTickets);
     }
 
     // subsequent getCombi
-    public static List<Integer> getCombi(List<Integer> previousCombi, int numConsecutiveTickets) {
+    private List<Integer> getCombi(List<Integer> previousCombi, int numConsecutiveTickets) {
         if (previousCombi.size() == numConsecutiveTickets) {
             return null;
         }
@@ -224,7 +237,8 @@ public class SeatsService {
         return toReturn;
     }
 
-    public static String returnUpdatedSeatsString(String seatsInformation, List<String> seats, char type) {
+    private String returnUpdatedSeatsString(String seatsInformation, List<String> seats, char type) {
+        char lookFor = (type == '1') ? '0' : '1';
         if (seats == null) {
             throw new IllegalArgumentException();
         }
@@ -232,126 +246,128 @@ public class SeatsService {
         for (int i = 0; i < seats.size(); i++) {
             String currentSeat = seats.get(i);
             int seatIndex = (currentSeat.charAt(0) - 'A') * NUM_ROWS + (currentSeat.charAt(1) - '1');
+//            if (updatedSeatsInformation.charAt(seatIndex) != lookFor) { throw new IllegalArgumentException(); }
             updatedSeatsInformation.setCharAt(seatIndex, type);
         }
 
         return updatedSeatsInformation.toString();
     }
 
-    public static void printSeatsMatrix(String seatsInformation) {
-        if (seatsInformation == null) {
-            System.out.println("No data");
-            return;
-        }
-        for (int i = 0; i < NUM_ROWS; i++) {
-            for (int j = 0; j < NUM_SEATS_PER_ROW; j++) {
-                System.out.print(seatsInformation.charAt(i * 4 + j) + " ");
-            }
-            System.out.println();
-        }
-    }
+//    private void printSeatsMatrix(String seatsInformation) {
+//        if (seatsInformation == null) {
+//            System.out.println("No data");
+//            return;
+//        }
+//        for (int i = 0; i < NUM_ROWS; i++) {
+//            for (int j = 0; j < NUM_SEATS_PER_ROW; j++) {
+//                System.out.print(seatsInformation.charAt(i * 4 + j) + " ");
+//            }
+//            System.out.println();
+//        }
+//    }
 
-    public static void printUpdatedSeatsMatrix(String seatsInformation, List<String> seats) {
-        if (seats == null) {
-            System.out.println("No seats available");
-            return;
-        }
-        StringBuilder updatedSeatsInformation = new StringBuilder(seatsInformation);
-        for (int i = 0; i < seats.size(); i++) {
-            String currentSeat = seats.get(i);
-            int seatIndex = (currentSeat.charAt(0) - 'A') * NUM_ROWS + (currentSeat.charAt(1) - '1');
-            updatedSeatsInformation.setCharAt(seatIndex, '2');
-        }
+//    private void printUpdatedSeatsMatrix(String seatsInformation, List<String> seats) {
+//        if (seats == null) {
+//            System.out.println("No seats available");
+//            return;
+//        }
+//        StringBuilder updatedSeatsInformation = new StringBuilder(seatsInformation);
+//        for (int i = 0; i < seats.size(); i++) {
+//            String currentSeat = seats.get(i);
+//            int seatIndex = (currentSeat.charAt(0) - 'A') * NUM_ROWS + (currentSeat.charAt(1) - '1');
+//            updatedSeatsInformation.setCharAt(seatIndex, '2');
+//        }
+//
+//        printSeatsMatrix(updatedSeatsInformation.toString());
+//    }
 
-        printSeatsMatrix(updatedSeatsInformation.toString());
-    }
-
-    private static void testCase(String seatsInformationAPIcall, int numTickets, int testCase) {
-        System.out.printf("------------------test %d--------------------\n", testCase);
-        System.out.printf("Seats to fill: %d\n", numTickets);
-        System.out.println("Original:");
-        printSeatsMatrix(seatsInformationAPIcall);
-
-        System.out.println("Updated:");
-        List<String> seats = getSeats(numTickets, seatsInformationAPIcall);
-        System.out.println(seats);
-        printUpdatedSeatsMatrix(seatsInformationAPIcall, seats);
-        System.out.println("--------------------------------------------");
-    }
+//    private void testCase(String seatsInformationAPIcall, int numTickets, int testCase) {
+//        System.out.printf("------------------test %d--------------------\n", testCase);
+//        System.out.printf("Seats to fill: %d\n", numTickets);
+//        System.out.println("Original:");
+//        printSeatsMatrix(seatsInformationAPIcall);
+//
+//        System.out.println("Updated:");
+//        List<String> seats = getSeats(numTickets, seatsInformationAPIcall);
+//        System.out.println(seats);
+//        printUpdatedSeatsMatrix(seatsInformationAPIcall, seats);
+//        System.out.println("--------------------------------------------");
+//    }
 
 
-    public static void main(String[] args) {
-        int testCase = 0;
-        {
-            testCase++;
-            StringBuilder seatsInformation = new StringBuilder();
-            seatsInformation.append("0000"); // row A
-            seatsInformation.append("0000"); // row B
-            seatsInformation.append("0000"); // row c
-            seatsInformation.append("0000"); // row D
-
-            testCase(seatsInformation.toString(), 1, testCase);
-        }
-        {
-            testCase++;
-            StringBuilder seatsInformation = new StringBuilder();
-            seatsInformation.append("1010"); // row A
-            seatsInformation.append("0101"); // row B
-            seatsInformation.append("1010"); // row c
-            seatsInformation.append("0101"); // row D
-
-            testCase(seatsInformation.toString(), 1, testCase);
-        }
-        {
-            testCase++;
-            StringBuilder seatsInformation = new StringBuilder();
-            seatsInformation.append("1111"); // row A
-            seatsInformation.append("0101"); // row B
-            seatsInformation.append("1010"); // row c
-            seatsInformation.append("0101"); // row D
-
-            testCase(seatsInformation.toString(), 1, testCase);
-        }
-        {
-            testCase++;
-            StringBuilder seatsInformation = new StringBuilder();
-            seatsInformation.append("1111"); // row A
-            seatsInformation.append("1100"); // row B
-            seatsInformation.append("1100"); // row c
-            seatsInformation.append("1111"); // row D
-
-            testCase(seatsInformation.toString(), 2, testCase);
-        }
-        {
-            testCase++;
-            StringBuilder seatsInformation = new StringBuilder();
-            seatsInformation.append("1111"); // row A
-            seatsInformation.append("1100"); // row B
-            seatsInformation.append("1100"); // row c
-            seatsInformation.append("1111"); // row D
-
-            testCase(seatsInformation.toString(), 4, testCase);
-        }
-        {
-            testCase++;
-            StringBuilder seatsInformation = new StringBuilder();
-            seatsInformation.append("1111"); // row A
-            seatsInformation.append("1001"); // row B
-            seatsInformation.append("1100"); // row c
-            seatsInformation.append("1111"); // row D
-
-            testCase(seatsInformation.toString(), 4, testCase);
-        }
-        {
-            testCase++;
-            StringBuilder seatsInformation = new StringBuilder();
-            seatsInformation.append("1111"); // row A
-            seatsInformation.append("1111"); // row B
-            seatsInformation.append("1111"); // row c
-            seatsInformation.append("1111"); // row D
-
-            testCase(seatsInformation.toString(), 4, testCase);
-        }
-    }
+//    public static void main(String[] args) {
+//        SeatsService seatsService = new SeatsService();
+//        int testCase = 0;
+//        {
+//            testCase++;
+//            StringBuilder seatsInformation = new StringBuilder();
+//            seatsInformation.append("0000"); // row A
+//            seatsInformation.append("0000"); // row B
+//            seatsInformation.append("0000"); // row c
+//            seatsInformation.append("0000"); // row D
+//
+//            testCase(seatsInformation.toString(), 1, testCase);
+//        }
+//        {
+//            testCase++;
+//            StringBuilder seatsInformation = new StringBuilder();
+//            seatsInformation.append("1010"); // row A
+//            seatsInformation.append("0101"); // row B
+//            seatsInformation.append("1010"); // row c
+//            seatsInformation.append("0101"); // row D
+//
+//            testCase(seatsInformation.toString(), 1, testCase);
+//        }
+//        {
+//            testCase++;
+//            StringBuilder seatsInformation = new StringBuilder();
+//            seatsInformation.append("1111"); // row A
+//            seatsInformation.append("0101"); // row B
+//            seatsInformation.append("1010"); // row c
+//            seatsInformation.append("0101"); // row D
+//
+//            testCase(seatsInformation.toString(), 1, testCase);
+//        }
+//        {
+//            testCase++;
+//            StringBuilder seatsInformation = new StringBuilder();
+//            seatsInformation.append("1111"); // row A
+//            seatsInformation.append("1100"); // row B
+//            seatsInformation.append("1100"); // row c
+//            seatsInformation.append("1111"); // row D
+//
+//            testCase(seatsInformation.toString(), 2, testCase);
+//        }
+//        {
+//            testCase++;
+//            StringBuilder seatsInformation = new StringBuilder();
+//            seatsInformation.append("1111"); // row A
+//            seatsInformation.append("1100"); // row B
+//            seatsInformation.append("1100"); // row c
+//            seatsInformation.append("1111"); // row D
+//
+//            testCase(seatsInformation.toString(), 4, testCase);
+//        }
+//        {
+//            testCase++;
+//            StringBuilder seatsInformation = new StringBuilder();
+//            seatsInformation.append("1111"); // row A
+//            seatsInformation.append("1001"); // row B
+//            seatsInformation.append("1100"); // row c
+//            seatsInformation.append("1111"); // row D
+//
+//            testCase(seatsInformation.toString(), 4, testCase);
+//        }
+//        {
+//            testCase++;
+//            StringBuilder seatsInformation = new StringBuilder();
+//            seatsInformation.append("1111"); // row A
+//            seatsInformation.append("1111"); // row B
+//            seatsInformation.append("1111"); // row c
+//            seatsInformation.append("1111"); // row D
+//
+//            testCase(seatsInformation.toString(), 4, testCase);
+//        }
+//    }
 }
 
