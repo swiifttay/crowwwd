cd219208f68d86ee4162844b6b0b31d8288dce9f
swiifttay <126874041+swiifttay@users.noreply.github.com>
2023-10-24 09:55:21 +0800

    test: fanrecords tests

--- backend/src/main/java/com/cs203/g1t4/backend/service/FanRecordService.java
+++ backend/src/main/java/com/cs203/g1t4/backend/service/FanRecordService.java
@@ -1,13 +1,17 @@
 package com.cs203.g1t4.backend.service;
 
 import com.cs203.g1t4.backend.data.request.fanRecord.FanRecordRequest;
+import com.cs203.g1t4.backend.data.response.Response;
 import com.cs203.g1t4.backend.data.response.common.SuccessResponse;
 import com.cs203.g1t4.backend.data.response.fanRecord.FanRecordResponse;
 import com.cs203.g1t4.backend.models.Artist;
 import com.cs203.g1t4.backend.models.FanRecord;
 import com.cs203.g1t4.backend.models.User;
 import com.cs203.g1t4.backend.models.exceptions.DuplicateFanRecordException;
+import com.cs203.g1t4.backend.models.exceptions.InvalidArtistIdException;
 import com.cs203.g1t4.backend.models.exceptions.InvalidTokenException;
+import com.cs203.g1t4.backend.models.exceptions.InvalidUsernameException;
+import com.cs203.g1t4.backend.repository.ArtistRepository;
 import com.cs203.g1t4.backend.repository.FanRecordRepository;
 import com.cs203.g1t4.backend.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -22,38 +26,11 @@ import java.util.Optional;
 public class FanRecordService {
     private final FanRecordRepository fanRecordRepository;
     private final UserRepository userRepository;
+    private final ArtistRepository artistRepository;
 
-    // not allowed to create a fan record manually because they should be via the spotify api
-//    public SuccessResponse createFanRecord(FanRecordRequest fanRecordRequest, String username) {
-//        // check if the user exists
-//        User user = userRepository.findByUsername(username)
-//                .orElseThrow(() -> new InvalidTokenException());
-//
-//        String userId = user.getId();
-//
-//        String artistId = fanRecordRequest.getArtistId();
-//
-//        // check if there is such a record already
-//        Optional<FanRecord> duplicateFanRecord = fanRecordRepository.findFanRecordByUserIdAndArtistId(artistId, userId);
-//
-//        if (duplicateFanRecord.isPresent()) {
-//            throw new DuplicateFanRecordException(artistId);
-//        }
-//
-//        FanRecord fanRecord = FanRecord.builder()
-//                .artistId(artistId)
-//                .userId(userId)
-//                .registerDate(LocalDateTime.now())
-//                .build();
-//
-//        return SuccessResponse.builder()
-//                .response("Fan Record successfully created")
-//                .build();
-//    }
-
-    public FanRecordResponse findAllFanRecordsUnderUser(String username) {
+    public Response findAllFanRecordsUnderUser(String username) {
         // get the user
-        User user = userRepository.findByUsername(username).orElseThrow(() -> new InvalidTokenException());
+        User user = userRepository.findByUsername(username).orElseThrow(() -> new InvalidUsernameException(username));
 
         // look for all the user id
         List<FanRecord> allFanRecordsUnderUser = fanRecordRepository.findFanRecordByUserId(user.getId());
@@ -68,12 +45,13 @@ public class FanRecordService {
     public void updateRecordsFromSpotify(List<String> topListsOfArtist, String username) {
         // check if the user exists
         User user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new InvalidTokenException());
+                .orElseThrow(() -> new InvalidUsernameException(username));
 
         String userId = user.getId();
 
 
         for (String artistId : topListsOfArtist) {
+            artistRepository.findById(artistId).orElseThrow(() -> new InvalidArtistIdException(artistId));
 
             // check if there is such a record already
             Optional<FanRecord> duplicateFanRecord = fanRecordRepository.findFanRecordByUserIdAndArtistId(userId, artistId);
