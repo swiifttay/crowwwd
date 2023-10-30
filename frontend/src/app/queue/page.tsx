22e2138ac990be4f8fc9438caa1ecbb4525e495e
yutinggh <huang.yu.tingting@gmail.com>
2023-10-30 13:58:44 +0800

    feat: queue slider automatic

--- frontend/src/app/queue/page.tsx
+++ frontend/src/app/queue/page.tsx
@@ -1,8 +1,41 @@
+"use client";
+
 import { Box, Slider } from "@mui/material";
+import { useEffect, useState } from "react";
 import Stepper from "../components/WaitingRoom/Stepper";
 import "./style.css";
 
 export default function Queue() {
+  const [sliderValue, setSliderValue] = useState(5);
+  const [peopleCount, setPeopleCount] = useState("2000+");
+
+  const updateSlider = () => {
+    setSliderValue((prevValue) => {
+      const newValue = prevValue + 1;
+      return newValue > 100 ? 0 : newValue;
+    });
+  };
+
+  useEffect(() => {
+    const interval = setInterval(updateSlider, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (sliderValue >= 0 && sliderValue < 20) {
+      setPeopleCount("2000+");
+    } else if (sliderValue >= 20 && sliderValue < 70) {
+      setPeopleCount("1000+");
+    } else if (sliderValue >= 70 && sliderValue < 80) {
+      setPeopleCount("500+");
+    } else {
+      setPeopleCount("100+");
+    }
+  }, [sliderValue]);
+
   return (
     <main className="flex flex-col items-center w-full h-screen px-8">
       <Stepper activeStep={2} />
@@ -19,12 +52,13 @@ export default function Queue() {
         }}
       >
         <div className="text-black justify-center text-center items-center align-middle flex flex-col p-12 lg:px-28 pb-16">
-          <div className="font-bold text-2xl mb-7">
+          <div className="font-bold text-2xl mb-6">
             You are now in the queue.
           </div>
-          <div className="font-bold text-7xl">2000+</div>
-          <div className="font-bold text-lg mb-6">people ahead of you</div>
-          <Slider defaultValue={10} aria-label="Default" />
+
+          <div className="font-bold text-8xl">{peopleCount}</div>
+          <div className="font-bold text-lg mb-5">people ahead of you</div>
+          <Slider value={sliderValue} aria-label="Default" />
         </div>
       </Box>
     </main>
